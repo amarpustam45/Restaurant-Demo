85a8ea056f7708f011477b91857e8c517aaf1a39
amarpustam45 <amarpustam@hotmail.com>
2021-10-13 15:09:58 -0400

    fixed menu and nav bar

--- javascript/functionality.js
+++ javascript/functionality.js
@@ -3,6 +3,5 @@ const navLinks = document.querySelector(".nav__link")
 const navLinkItem = document.querySelectorAll(".nav__link-item") 
 
 hamburger.addEventListener("click", () => {
-
     navLinks.classList.toggle("open")
 })
